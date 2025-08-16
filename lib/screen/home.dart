ab049d756af65411db2bee8d596392e2be1f04f3
Supakit <beam0820234866@gmail.com>
2025-08-16 11:08:39 +0700

    Step 5 Image widget

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -6,51 +6,13 @@ class Home extends StatelessWidget {
     // return Row(
     return Column(
       mainAxisAlignment: MainAxisAlignment.start,
-      // mainAxisAlignment: MainAxisAlignment.center,
-      // mainAxisAlignment: MainAxisAlignment.end,
-      // mainAxisAlignment: MainAxisAlignment.spaceAround,
-      // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      // crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Container(
-          color: Colors.redAccent,
-          padding: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-          child: Text(
-            "Hello",
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 3,
-            ),
-          ),
-        ),
-        Container(
-          color: Colors.redAccent,
-          padding: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-          child: Text(
-            "Hello",
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 3,
-            ),
-          ),
-        ),
-        Container(
-          color: Colors.redAccent,
-          padding: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-          child: Text(
-            "Hello",
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 3,
-            ),
-          ),
+        Image.asset("assets/images/CUTE-KITTY.jpg", width: 200, height: 200),
+        const SizedBox(height: 20),
+        Image.network(
+          "https://www.central.co.th/e-shopping/wp-content/uploads/2020/12/CUTE-KITTY.jpg",
+          width: 200,
+          height: 200,
         ),
       ],
     );
